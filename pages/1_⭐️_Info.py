3fc39869bfc1f363db3b40c7bfea96d4fce9a7c6
Tuana Celik <tuana.celik@deepset.ai>
2022-12-15 10:49:07 +0100

    re-wording

--- pages/1_⭐️_Info.py
+++ pages/1_⭐️_Info.py
@@ -4,12 +4,12 @@ from utils.frontend import build_sidebar
 build_sidebar()
 
 st.markdown("""
-# Better Image Retrieval With Reinforced CLIP 🧠
+# Better Image Retrieval With Retrieval-Augmented CLIP 🧠
 
 
 CLIP is a neural network trained on image-text pairs that can predict how semantically close images are with some text
 But, although CLIP understands what it sees, it doesn't know its properties. While other models can understand text that contains such information, like Wikipedia.
 
-In this demo application, we see if we can 'help' CLIP by reinforcing it with another model.""")
+In this demo application, we see if we can 'help' CLIP with another model.""")
 
 st.image("diagram.png")
