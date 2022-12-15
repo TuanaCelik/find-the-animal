98abdf8b082e9d9eef905bf00c87ff0645276d21
Tuana Celik <tuana.celik@deepset.ai>
2022-12-15 12:14:24 +0100

    updated sidebar

--- utils/frontend.py
+++ utils/frontend.py
@@ -2,7 +2,9 @@ import streamlit as st
 
 def build_sidebar():
     sidebar = """
+    <div style='text-align: center'>
     <p><br/><a href='https://github.com/TuanaCelik/find-the-animal'>Github project</a> - Based on <a href='https://github.com/deepset-ai/haystack'>Haystack</a></p>
+    <p>Project by <a href='https://github.com/ZanSara'>Sara Zanzottera</a> and <a href='https://github.com/TuanaCelik'>Tuana Celik</a></p>
     </div>
     """
     st.sidebar.markdown(sidebar, unsafe_allow_html=True)
