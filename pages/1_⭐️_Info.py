f32e4f11274cbe040b5be5aaf554a69e75b6ff99
ZanSara <sarazanzo94@gmail.com>
2022-12-15 13:52:58 +0100

    nodes

--- pages/1_⭐️_Info.py
+++ pages/1_⭐️_Info.py
@@ -33,17 +33,28 @@ st.markdown("""
 In the image above you can see how the process looks like.
 
 First, we download a slice of Wikipedia with information about all the animals in the Lisbon zoo and preprocess,
-index, embed and store them.
+index, embed and store them in a DocumentStore. For this demo we're using 
+[FAISSDocumentStore](https://docs.haystack.deepset.ai/docs/document_store).
 
-At this point they are ready to be queried by the text Retriever, which compares the user's question ("The fastest animal")
-to all the documents indexed earlier and returns the documents which are more likely to contain an answer to the question.
+At this point they are ready to be queried by the text Retriever, in this case an instance of 
+[EmbeddingRetriever](https://docs.haystack.deepset.ai/docs/retriever#embedding-retrieval-recommended).
+It compares the user's question ("The fastest animal") to all the documents indexed earlier and returns the 
+documents which are more likely to contain an answer to the question.
 In this case, it will probably return snippets from the Cheetah Wikipedia entry.
 
-Once the documents are found, they are handed over to the Reader, a model that is able to locate precisely the answer to a
-question into a document. These answers are strings that should be now very easy for CLIP to understand, such as the name of an animal.
+Once the documents are found, they are handed over to the Reader (in this demo, a 
+[FARMReader](https://docs.haystack.deepset.ai/docs/reader) node): 
+a model that is able to locate precisely the answer to a question into a document. 
+These answers are strings that should be now very easy for CLIP to understand, such as the name of an animal.
 In this case, the Reader will return answers such as "Cheetah", "the cheetah", etc.
 
-These strings are then ranked and the most likely one is sent over to CLIP, which will use its own document store of images
-to find all the pictures that match the string. Cheetah are present in the Lisbon zoo, so it will find pictures of them and
-return them.
+These strings are then ranked and the most likely one is sent over to the 
+[MultiModalRetriever](https://docs.haystack.deepset.ai/docs/retriever#multimodal-retrieval) 
+that contains CLIP, which will use its own document store of images to find all the pictures that match the string. 
+Cheetah are present in the Lisbon zoo, so it will find pictures of them and return them.
+
+These nodes are chained together using a Pipeline object, so that all you need to do to run
+a system like this is a single call: `pipeline.run(query="What's the fastest animal?")` 
+will return the list of images directly. 
+Have a look at [how we implemented it](https://github.com/TuanaCelik/find-the-animal/blob/main/utils/haystack.py)!
 """)
