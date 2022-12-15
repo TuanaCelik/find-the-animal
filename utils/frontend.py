868805e6f926a73eb4678189426293325630e664
Tuana Celik <tuana.celik@deepset.ai>
2022-12-15 12:27:17 +0100

    extend sidebar

--- utils/frontend.py
+++ utils/frontend.py
@@ -5,6 +5,8 @@ def build_sidebar():
     <div style='text-align: center'>
     <p><br/><a href='https://github.com/TuanaCelik/find-the-animal'>Github project</a> - Based on <a href='https://github.com/deepset-ai/haystack'>Haystack</a></p>
     <p>Project by <a href='https://github.com/ZanSara'>Sara Zanzottera</a> and <a href='https://github.com/TuanaCelik'>Tuana Celik</a></p>
+    <p>Project based on the "Introduction to Image Retrieval" presentation by Sara at the <a href="https://www.meetup.com/open-nlp-meetup/events/289499354/">Open NLP Meetup</a></p>
+    <p><a href="https://www.youtube.com/watch?v=7Idjl3OR0FY">Watch the presentation</a></p>
     </div>
     """
     st.sidebar.markdown(sidebar, unsafe_allow_html=True)
